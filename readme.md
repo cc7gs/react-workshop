babd9c0d453e52dd6434b0ae2c22a22548eb6e4b
wuchen <chenorange12@gmail.com>
2019-09-28 17:26:36 +0800

    转换为 jsx

--- readme.md
+++ readme.md
@@ -1,5 +1,7 @@
 > 本仓库是学习 frontendMaster react-v5学习笔记
 
+** 可以根据我提交记录去查看相关内容
+
 [原仓库地址](https://github.com/btholt/complete-intro-to-react-v5)
 [原文档地址](https://btholt.github.io/complete-intro-to-react-v5)
 
--- src/app.js
+++ src/app.js
@@ -1,17 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-const Pet = ({ name, animal, breed }) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed)
-  ]);
-};
+import Pet from './Pet'
+
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt me"),
-    React.createElement(Pet, { name: "Luna", animal: "dog", breed: "Havanes" }),
-    React.createElement(Pet, { name: "Pepper", animal: "Bird", breed: "ha" })
-  ]);
-};
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+  return (
+    <div>
+      <h1>dopt me</h1>
+      <Pet
+        name="Luna"
+        animal="dog"
+        breed="Havanes" />
+    </div>
+  )
+}
+ReactDOM.render(<App/>, document.getElementById("root"));
