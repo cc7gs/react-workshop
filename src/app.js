db78ab835128c2c0914e709772b3aa0befeae93a
wuchen <chenorange12@gmail.com>
2019-09-28 16:13:20 +0800

    react init

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const Pet = ({ name, animal, breed }) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed)
+  ]);
+};
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt me"),
+    React.createElement(Pet, { name: "Luna", animal: "dog", breed: "Havanes" }),
+    React.createElement(Pet, { name: "Pepper", animal: "Bird", breed: "ha" })
+  ]);
+};
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
