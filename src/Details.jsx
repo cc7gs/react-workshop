bce032e23b56523ac6e66dac7c4b11591856e4b2
wuchen <chenorange12@gmail.com>
2019-10-03 00:02:48 +0800

    添加 详情页面

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,10 +1,46 @@
-import React from 'react'
+import React, { Component } from 'react'
+import pet from "@frontendmasters/pet";
 
-const Details=()=>{
-    return (
-        <div>
-           details page 
-        </div>
-    )
+export default class Details extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            loading: true
+        }
+    }
+    
+    componentDidMount() {
+        pet.animal(+this.props.id)
+            .then(({ animal }) => {
+                this.setState({
+                    name: animal.name,
+                    animal: animal.type,
+                    location: `${animal.contact.address.city}, ${
+                        animal.contact.address.state
+                        }`,
+                    description: animal.description,
+                    media: animal.photos,
+                    breed: animal.breeds.primary,
+                    loading: false
+                })
+            })
+            .catch(err => this.setState({ error: err }))
+    }
+    render() {
+        if (this.state.loading) {
+            return <h1>loading … </h1>;
+        }
+        const {animal,breed,location,name,description}=this.state
+        return (
+            <div className="details">
+                <div>
+                    <h1>{name}</h1>
+                    <h2>{`${animal} — ${breed} — ${location}`}</h2>
+                    <button>Adopt {name}</button>
+                    <p>{description}</p>
+                </div>
+            </div>
+        )
+    }
 }
-export default Details
+
