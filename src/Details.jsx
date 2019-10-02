692a3d2a2822752036b4c75a9fed58525c2be356
wuchen <chenorange12@gmail.com>
2019-10-02 23:39:00 +0800

    add reach router

---
+++ src/Details.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Details=()=>{
+    return (
+        <div>
+           details page 
+        </div>
+    )
+}
+export default Details
--- src/app.js
+++ src/app.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Router, Link } from "@reach/router";
 import SearchParams from './SearchParams'
-
+import Details from './Details'
 const App = () => {
   return (
     <div>
-      <h1>dopt me</h1>
-        <SearchParams/>
+      <header>
+        <Link to="/">dopt me</Link>
+      </header>
+      <Router>
+        <SearchParams path="/" />
+        <Details path="/details/:id" />
+      </Router>
     </div>
   )
 }
-ReactDOM.render(<App/>, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
